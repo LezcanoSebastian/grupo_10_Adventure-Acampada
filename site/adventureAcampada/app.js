af730d568e2e2817bbc8c4ae678aae888464c19e
EliMC-Nag <eliana.naguila@gmail.com>
2021-02-07 17:14:07 -0300

    Se crea ruta para el archivo Formulario de Producto

--- site/adventureAcampada/app.js
+++ site/adventureAcampada/app.js
@@ -7,8 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 /* rutas */
-var detalleRouter = require('./routes/detalle')
-var carritoRouter = require('./routes/carrito')
+var detalleRouter = require('./routes/detalle');
+var carritoRouter = require('./routes/carrito');
+var formularioRouter = require('./routes/formProduct');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/detalle', detalleRouter);
 app.use('/carrito', carritoRouter);
+app.use('/formProduct', formularioRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
