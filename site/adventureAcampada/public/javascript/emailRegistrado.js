454f24131fa6cf2649968eab295fb9a43bf460da
sebastianLezcano <sebastian.e.lezcano@gmail.com>
2021-04-26 23:21:45 -0300

    Persistencia de errores en formularios de productos

--- site/adventureAcampada/public/javascript/emailRegistrado.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", () => {
-    let  emailRegistrado = qs(".emailRegistrado")
-    const delay = 1000
-    setTimeout(email, delay);
-        function email(){
-            emailRegistrado.style.display = "none"
-        }
-})
-
